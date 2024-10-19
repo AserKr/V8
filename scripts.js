cf4deb5baa718615d643c527e88959937069883b
AserKr <asser.krouma232@gmail.com>
2024-10-19 14:09:14 +0000

    Assignmnet 8

--- scripts.js
+++ scripts.js
@@ -1,18 +1,44 @@
-/**
- * Sýnilausn á verkefni 8 í Vefforritun 1, 2024.
- * Byggir á sýnilausn á verkefni 7.
- * Notar jsdoc til að skrifa lýsingu á föllum, inntaki og úttaki.
- * Kveikið á `Check JS` í Visual Studio Code til að sjá mögulegar villur.
- * Notar `console.assert` til að athuga hvort föll virki rétt.
- */
+import { splitOnWhitespace } from "./lib/helpers.js";
+import { longest as longestFunction } from "./lib/longest.js";
+import { shortest as shortestFunction } from "./lib/shortest.js";
+import { vowels as vowelsFunction } from "./lib/vowels.js";
+import { consonants as consonantsFunction } from "./lib/consonants.js";
+import { reverse as reverseFunction } from "./lib/reverse.js";
+import { palindrome as palindromeFunction } from "./lib/palindrome.js";
 
-import { isString, splitOnWhitespace } from './lib/helpers.js';
+const textareaElement = document.querySelector("textarea");
+const formElement = document.querySelector("form");
+const codeElement = document.querySelector("code .input");
+const container = document.querySelector(".hidden");
+const longestElement = document.querySelector(".longest");
+const shortestElement = document.querySelector(".shortest");
+const vowelsElement = document.querySelector(".vowels");
+const consonantsElement = document.querySelector(".consonants");
+const reverseElement = document.querySelector(".reversed");
+const palindromeElement = document.querySelector(".palindrome");
 
-const test = isString('hæ');
-console.log('test er strengur?', test);
+function submitHandler(event) {
+  event.preventDefault();
+  console.log("submit", event);
+}
+formElement.addEventListener("submit", submitHandler);
 
-const stringWithWhitespace = `halló
-\theimur
-hæ`;
-const split = splitOnWhitespace(stringWithWhitespace);
-console.log(split);
+textareaElement.addEventListener("input", (event) => {
+  const text = event.target.value.trim();
+
+  codeElement.textContent = text;
+
+  if (splitOnWhitespace(text).length > 0) {
+    container.classList.remove("hidden");
+  } else {
+    container.classList.add("hidden");
+  }
+
+  // Update content based on current input
+  longestElement.textContent = longestFunction(text);
+  shortestElement.textContent = shortestFunction(text);
+  vowelsElement.textContent = vowelsFunction(text);
+  consonantsElement.textContent = consonantsFunction(text);
+  palindromeElement.textContent = palindromeFunction(text) ? "" : "ekki";
+  reverseElement.textContent = reverseFunction(text);
+});
