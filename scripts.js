a27baf9aed5a22963120c7b6c74ae034a215f476
AserKr <asser.krouma232@gmail.com>
2024-10-19 21:54:59 +0000

    Added javadoc for all the js files

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
+/**
+ * Sýnilausn á verkefni 8 í Vefforritun 1, 2024.
+ * Byggir á sýnilausn á verkefni 7.
+ * Notar jsdoc til að skrifa lýsingu á föllum, inntaki og úttaki.
+ * Kveikið á `Check JS` í Visual Studio Code til að sjá mögulegar villur.
+ * Notar `console.assert` til að athuga hvort föll virki rétt.
+ */
+
+/**
+ * Import statements for the functions needed in this file
+ */
 import { splitOnWhitespace } from "./lib/helpers.js";
 import { longest as longestFunction } from "./lib/longest.js";
 import { shortest as shortestFunction } from "./lib/shortest.js";
@@ -6,6 +17,10 @@ import { consonants as consonantsFunction } from "./lib/consonants.js";
 import { reverse as reverseFunction } from "./lib/reverse.js";
 import { palindrome as palindromeFunction } from "./lib/palindrome.js";
 
+/**
+ *
+ * Queries DOM elements for interaction in the listeners below.
+ */
 const textareaElement = document.querySelector("textarea");
 const formElement = document.querySelector("form");
 const codeElement = document.querySelector("code .input");
@@ -17,12 +32,33 @@ const consonantsElement = document.querySelector(".consonants");
 const reverseElement = document.querySelector(".reversed");
 const palindromeElement = document.querySelector(".palindrome");
 
+/**
+ * Handles the form submission event and Prevents default form behavior and logs the textarea value.
+ *
+ *
+ * @param {Event} event The submit event triggered by the form.
+ */
 function submitHandler(event) {
   event.preventDefault();
-  console.log("submit", event);
+  const { target } = event;
+  const textareaElement = target.querySelector("textarea");
+  console.log("textareaElement has", textareaElement.value);
 }
+
+/**
+ * Adds an event listener to the form for handling submission.
+ */
 formElement.addEventListener("submit", submitHandler);
 
+/**
+ * Adds an event listener to the textarea element to handle input changes.
+ * Updates the text elements based on the user's input.
+ * - Shows/hides the container based on whether text is present, shows it when there is at least 1 string in the textarea element.
+ * - Displays the longest and shortest words, the count of vowels and consonants, and whether the text is a palindrome.
+ * - Reverses the input text.
+ *
+ * @param {Event} event The input event triggered when the user types in the textarea.
+ */
 textareaElement.addEventListener("input", (event) => {
   const text = event.target.value.trim();
 
@@ -34,7 +70,6 @@ textareaElement.addEventListener("input", (event) => {
     container.classList.add("hidden");
   }
 
-  // Update content based on current input
   longestElement.textContent = longestFunction(text);
   shortestElement.textContent = shortestFunction(text);
   vowelsElement.textContent = vowelsFunction(text);
